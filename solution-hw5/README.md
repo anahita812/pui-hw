947da0588213af7ee6854b6969afcf7f44e1b654
anahita812 <112357939+anahita812@users.noreply.github.com>
2022-10-06 16:27:03 -0400

    readme

--- solution-hw5/README.md
+++ solution-hw5/README.md
@@ -1,8 +1,8 @@
 Readme:
-    Deployed website: https://anahita812.github.io/pui-hw/solution-hw4/productgallery.html
-    Repository link: https://github.com/anahita812/pui-hw/tree/main/solution-hw4
+    Deployed website: https://anahita812.github.io/pui-hw/solution-hw5/productgallery.html
+    Repository link: https://github.com/anahita812/pui-hw/tree/main/solution-hw5
 
 Citations:
-    https://www.w3schools.com/js/js_objects.asp
-    https://www.w3schools.com/jsref/event_onclick.asp
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+    https://stackoverflow.com/questions/59929042/how-to-create-function-inside-class-in-javascript
 
--- solution-hw5/js/shoppingcart.js
+++ solution-hw5/js/shoppingcart.js
@@ -28,10 +28,10 @@ class Roll {
 
 //Code to update cart1 with 4 rolls 
 for (let i = 0; i < 1; i++) {
-    additem1()
-    additem2()
-    additem3()
-    additem4()
+    updatecart("Sugar milk", "1", "Original")
+    updatecart("Vanilla milk", "12", "Walnut")
+    updatecart("Sugar milk", "3", "Raisin")
+    updatecart("Keep original", "3", "Apple")
     //add parameters
 }
 
@@ -42,38 +42,6 @@ function addtocart() {
     let addedRoll = new Roll(currentrollType, glaze, packsizenum, currentprice);
     cart.push(addedRoll);
     console.log("cart", cart);
-}
-
-//function for updating array
-function additem1() {
-    let glaze = "Sugar milk";
-    let packsizenum = "1";
-    let rollselect = "Original"
-    updatecart(glaze, packsizenum, rollselect);
-}
-
-//function for updating array
-function additem2() {
-    let glaze = "Vanilla milk";
-    let packsizenum = "12";
-    let rollselect = "Walnut"
-    updatecart(glaze, packsizenum, rollselect);
-}
-
-//function for updating array
-function additem3() {
-    let glaze = "Sugar milk";
-    let packsizenum = "3";
-    let rollselect = "Raisin"
-    updatecart(glaze, packsizenum, rollselect);
-}
-
-//function for updating array
-function additem4() {
-    let glaze = "Keep original";
-    let packsizenum = "3";
-    let rollselect = "Apple";
-    updatecart(glaze, packsizenum, rollselect);
 }
 
 //function to update and push
@@ -100,20 +68,20 @@ function displaycart(rollitem) {
     let image = rolls[rollitem.type].imageFile;
     this.imagereplace.src = './products/' + image;
     this.rightside = cloned.querySelector('.kidsofcontainer1');
-    let cinnamonrolename =  this.rightside.querySelector('.cinnamonroll');
+    let cinnamonrolename = this.rightside.querySelector('.cinnamonroll');
     cinnamonrolename.innerText = rollitem.type + " Cinammon Roll";
-    let glazingholder =  this.rightside.querySelector('.glazing');
+    let glazingholder = this.rightside.querySelector('.glazing');
     glazingholder.innerText = "Glazing: " + rollitem.glazing;
-    let packholder =  this.rightside.querySelector('.pack');
+    let packholder = this.rightside.querySelector('.pack');
     packholder.innerText = "Pack Size: " + rollitem.size;
     this.priceupdate = cloned.querySelector('.kidsofcontainer');
     this.priceupdate.innerText = "$ " + rollitem.calculatedprice;
     let button = cloned.querySelector('.smallertext');
     console.log(button);
     button.addEventListener('click', () => {
-        removecartitem(cloned,rollitem);
+        removecartitem(cloned, rollitem);
     });
-    packholder.innerText = "Pack Size: " +rollitem.size;
+    packholder.innerText = "Pack Size: " + rollitem.size;
     entry.append(cloned);
     updatetotalprice()
 }
@@ -122,11 +90,11 @@ function displaycart(rollitem) {
 function updatetotalprice() {
     let price = 0
     for (let i = 0; i < cart1.length; i++) {
-    price = price + parseFloat(cart1[i].calculatedprice);
-}
-    let priceholder =  document.querySelector('.sep1');
+        price = price + parseFloat(cart1[i].calculatedprice);
+    }
+    let priceholder = document.querySelector('.sep1');
     priceholder.innerText = "$" + price;
-    console.log("price",price)
+    console.log("price", price)
 }
 //initialize cart1
 for (let i = 0; i < cart1.length; i++) {
@@ -135,26 +103,15 @@ for (let i = 0; i < cart1.length; i++) {
 }
 
 //removefunction
-function removecartitem(cloned,rollitem) {
+function removecartitem(cloned, rollitem) {
     console.log("remover");
     for (let i = 0; i < cart1.length; i++) {
-        if (cart1[i]==rollitem) {
-           cart1.splice(i,1);
-           break;
-          }
+        if (cart1[i] == rollitem) {
+            cart1.splice(i, 1);
+            break;
+        }
     }
     console.log(cart1);
     cloned.remove();
     updatetotalprice();
 }
-
-
-/*
-Remove items from the cart
-When users click the “Remove” link at any entry,  
-the item should be removed from the cart array or set, 
-the corresponding Roll entry should be removed from the DOM, 
-and the price total at the bottom should be updated. 
-Make sure to test your implementation against edge cases, e.g., when your cart is empty (cart.length == 0).
-*/
-
